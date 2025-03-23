c382e5ba850962ff9077e0fda2dd6af4b391814e
NGOC HAN <NGOC HAN@DESKTOP-RJACTB4>
2025-03-23 10:27:16 +0700

    Restore

---
+++ Bookshop Management System/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bookshop
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            // Khởi tạo và hiển thị frmLogin trước
+            Login loginForm = new Login();
+            if (loginForm.ShowDialog() == DialogResult.OK)
+            {
+                // Nếu đăng nhập thành công, mở frmMenu
+                Application.Run(new Menu());
+            }
+        }
+    }
+}
